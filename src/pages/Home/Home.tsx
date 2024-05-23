bb7d034d9e0bc366e184f178fc62afd849246560
Guillermo Sotelo <guillermo.sotelo@volvocars.com>
2024-05-23 17:45:41 +0200

    mobile

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -59,7 +59,7 @@ export default function Home({ }: Props) {
     a.click()
   }
 
-  const renderDesktop = () => {
+  const renderHome = () => {
     return (
       <div className='page__row'>
         <div className='home__container'>
@@ -197,44 +197,5 @@ export default function Home({ }: Props) {
     )
   }
 
-  const renderMobile = () => {
-    return (
-      <div className='page__row'>
-        <div className='home__container'>
-          <div className="home__section">
-            <div className="home__landing">
-              <div className="home__landing-text">
-                <div className="home__landing-text-col">
-                  <h1 className="home__landing-title">Equilibrio<br />y Superación</h1>
-                  <h2 className="home__landing-subtitle">Psicología y entrenamiento en habilidades</h2>
-                  <div style={{ textAlign: 'center', width: '100vw' }}>
-                    <p className='home__landing-cta'>Tu primer paso hacia una vida mejor</p>
-                    <Button
-                      label='Reserva una cita'
-                      handleClick={() => history.push('/booking')}
-                      style={{
-                        transform: 'scale(1.5)',
-                        height: 'fit-content',
-                        width: 'fit-content',
-                        opacity: 0,
-                        animation: 'fade-in 2s ease forwards',
-                        animationDelay: '1.5s'
-                      }}
-                      bgColor={APP_COLORS.SKY}
-                    />
-                  </div>
-                </div>
-                <div className="home__landing-text-col" style={{ textAlign: 'left', alignItems: 'center' }}>
-                </div>
-              </div>
-              <div className="home__landing-img-gradient" />
-              <img src={BG} alt="Equilibrio y Superación (imagen)" className="home__landing-img" />
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-  return isMobile ? renderMobile() : renderDesktop()
+  return renderHome()
 }
